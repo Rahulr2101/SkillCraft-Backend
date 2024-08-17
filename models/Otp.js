8fc04f791a4a505b8376d1ae028218bb71935829
Rahul <rahulrajesh@2101.gmail.com>
2024-08-17 22:55:59 +0530

    feat: add pre-middleware for email verification.

--- models/Otp.js
+++ models/Otp.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const sendEmail = require("../utils/mailSender");
 
 const OTP = new mongoose.Schema({
   email: {
@@ -15,5 +16,20 @@ const OTP = new mongoose.Schema({
     expires: 600,
   },
 });
+
+async function sendVerificationEmail(email,otp){
+  try{
+    const mailResponse = await sendEmail(email,"OTP for verification",`<h1>Your OTP is ${otp}</h1>`);
+    console.log("mail sent successfully:",mailResponse);
+
+  }catch(err){
+    console.log("error occured while sending mails:",err);
+  }
+}
+
+OTP.pre("save",async function(next){
+  await sendVerificationEmail(this.email,this.otp);
+  next();
+})
 
 module.exports = mongoose.model("OTP", OTP);
