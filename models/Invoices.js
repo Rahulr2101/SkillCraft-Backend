51e8ca6e160fc5336225a772c62c9081061d0fdc
Rahul <rahulrajesh@2101.gmail.com>
2024-08-17 21:43:03 +0530

    feat: add otp model with attributes.

--- models/Invoices.js
+++ models/Invoices.js
@@ -1,28 +1,27 @@
 const mongoose = require("mongoose");
 
 const invoice = new mongoose.Schema({
-    User:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"User",
-    },
-    couseName:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"Course"
-    },
-    price:{
-        type:String
-    },
-    address:{
-        type:String
-    },
-    pincode:{
-        type:String
-    },
-    courseID:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"Course"
-    }
-    
-})
+  User: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  couseName: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Course",
+  },
+  price: {
+    type: String,
+  },
+  address: {
+    type: String,
+  },
+  pincode: {
+    type: String,
+  },
+  courseID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Course",
+  },
+});
 
 module.exports = mongoose.model("Invoice", invoice);
