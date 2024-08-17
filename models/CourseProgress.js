acdcbd188ffbf43320fc90a9be2b6fb770ce978e
Rahul <rahulrajesh@2101.gmail.com>
2024-08-17 21:00:03 +0530

    feat: add SubSection model with attributes.

--- models/CourseProgress.js
+++ models/CourseProgress.js
@@ -1,13 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const courseProgress = new mongoose.Schema({
-    coursID:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"Course",
+  coursID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Course",
+  },
+  completedVideos: [
+    {
+      type: mongoose.Schema.Types,
+      ref: "SubSection",
     },
-    completedVideos:[{
-        type:mongoose.Schema.Types,
-        ref:"SubSection"
-    }],
-    
-})
+  ],
+});
+
+module.exports = mongoose.model("CourseProgress", courseProgress);
